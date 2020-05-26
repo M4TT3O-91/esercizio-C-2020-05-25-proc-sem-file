b651dcebf7fefe615e79cbe326ed41e113eaf29d
M4TT3O-91 <teo_exc@hotmail.it>
2020-05-26 09:43:06 +0200

    update

--- src/esercizio-C-2020-05-25-proc-sem-file.c
+++ src/esercizio-C-2020-05-25-proc-sem-file.c
@@ -52,13 +52,174 @@
 sem_t *proc_sem;
 sem_t *mutex;
 
-int create_file_set_size(char *file_name, unsigned int file_size);
-void soluzione_A();
-void read_write_in_file(int *fd, int i ,int eof);
+//int create_file_set_size(char *file_name, unsigned int file_size);
+//void soluzione_A();
+//void read_write_in_file(int fd, int i);
 
 #define CHECK_ERR(a,msg) {if ((a) == -1) { perror((msg)); exit(EXIT_FAILURE); } }
 #define CHECK_ERR_MMAP(a,msg) {if ((a) == MAP_FAILED) { perror((msg)); exit(EXIT_FAILURE); } }
 
+void soluzione_A() {
+//	usare le system call open(), lseek(), write()
+	char *file_name = "output_A.txt";
+	unsigned int file_size = FILE_SIZE;
+	int fd = create_file_set_size(file_name, file_size);
+	pid_t pid;
+
+	for (int i = 0; i < N; i++) {
+		pid = fork();
+		switch (pid) {
+		case 0:
+			while (1) {
+				if (sem_wait(proc_sem) == -1) {
+					perror("sem_wait");
+					exit(EXIT_FAILURE);
+				}
+				read_write_in_file(fd, i);
+			}
+			break;
+		case -1:
+			perror("fork()");
+			exit(EXIT_FAILURE);
+		default:
+			;
+		}
+	}
+	for (int i = 0; i < FILE_SIZE + N; i++) {
+		if (sem_post(proc_sem) == -1) {
+			perror("sem_post");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for (int j = 0; j < N; j++)
+		wait(NULL);
+
+	printf("Processo completato BYE!\n");
+	return;
+}
+
+void soluzione_B() {
+//	usare le system call open(), lseek(), write()
+	char *file_name = "output_B.txt";
+	unsigned int file_size = FILE_SIZE;
+	int file_map;
+	int res;
+	int fd = create_file_set_size(file_name, file_size);
+	char append_message;
+
+	pid_t pid;
+
+	file_map = mmap(NULL, // NULL: è il kernel a scegliere l'indirizzo
+			file_size, // dimensione della memory map
+			PROT_READ | PROT_WRITE, // memory map leggibile e scrivibile
+			MAP_SHARED, // memory map condivisibile con altri processi
+			fd, 0); // offset nel file
+
+	CHECK_ERR_MMAP(file_map, "file mapping error");
+	close(fd);
+
+	for (int i = 0; i < N; i++) {
+		pid = fork();
+		switch (pid) {
+		case 0:
+			append_message = 'A' + (char) i;
+			for (int j = 0; j < FILE_SIZE; j++) {
+				if (sem_wait(proc_sem) == -1) {
+					perror("sem_wait");
+					exit(EXIT_FAILURE);
+				}
+
+				if (sem_wait(mutex) == -1) {
+					perror("sem_wait");
+					exit(EXIT_FAILURE);
+				}
+				if (file_map[j] == 0) {
+					printf("CHILD %d scrive in posizione %d\n", i);
+					res = write(fd, &append_message, sizeof(append_message));
+					CHECK_ERR(res, "Write error")
+
+					if (sem_post(mutex) == -1) {
+						perror("sem_post");
+						exit(EXIT_FAILURE);
+					}
+				}
+			}
+			break;
+		case -1:
+			perror("fork()");
+			exit(EXIT_FAILURE);
+		default:
+			;
+		}
+	}
+	for (int i = 0; i < FILE_SIZE + N; i++) {
+		if (sem_post(proc_sem) == -1) {
+			perror("sem_post");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for (int j = 0; j < N; j++)
+		wait(NULL);
+
+	printf("Processo completato BYE!\n");
+	return;
+}
+
+int create_file_set_size(char *file_name, unsigned int file_size) {
+	int res;
+	int fd = open(file_name,
+	O_CREAT | O_TRUNC | O_RDWR,
+	S_IRUSR | S_IWUSR // l'utente proprietario del file avrà i permessi di lettura e scrittura sul nuovo file
+	);
+
+	CHECK_ERR(fd, "open file")
+
+	res = ftruncate(fd, file_size);
+	CHECK_ERR(res, "ftruncate()")
+
+	return fd;
+}
+
+void read_write_in_file(int fd, int i) {
+
+	char append_message = 'A' + (char) i;
+	char tmp;
+	int res;
+	int curr;
+
+	if (sem_wait(mutex) == -1) {
+		perror("sem_wait");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((res = read(fd, &tmp, 1)) > 0 && tmp != 0)
+		;
+	if (res <= 0) {
+		if (sem_post(mutex) == -1) {
+			perror("sem_post");
+			exit(EXIT_FAILURE);
+		}
+		printf("CHILD numero %d EXIT_SUCCESS\n", i);
+		exit(EXIT_SUCCESS);
+	}
+	res = lseek(fd, -1, SEEK_CUR);
+	CHECK_ERR(res, "Lseek");
+
+	printf("CHILD %d scrive a offset %d\n", i, res);
+	res = write(fd, &append_message, sizeof(append_message));
+	CHECK_ERR(res, "Write error")
+
+	if (sem_post(mutex) == -1) {
+		perror("sem_post");
+		exit(EXIT_FAILURE);
+	}
+
+	return;
+}
+
+/******************************************************************************************/
 int main(int argc, char *argv[]) {
 
 	int res;
@@ -85,115 +246,9 @@ int main(int argc, char *argv[]) {
 	soluzione_A();
 
 	printf("ed ora avvio la soluzione_B()...\n");
-	//soluzione_B();
+	soluzione_B();
 
 	printf("bye!\n");
 	return 0;
 }
 
-void soluzione_A() {
-//	usare le system call open(), lseek(), write()
-	char *file_name = "output_A.txt";
-	unsigned int file_size = FILE_SIZE;
-	int fd = create_file_set_size(file_name, file_size);
-
-	pid_t pid;
-
-
-	int eof = lseek(fd, 0, SEEK_END);
-	CHECK_ERR(eof, "Lseek error")
-
-	int res = lseek(fd, 0, SEEK_SET);
-	CHECK_ERR(res, "Lseek error")
-
-	for (int i = 0; i < N; i++) {
-		pid = fork();
-		switch (pid) {
-		case 0:
-			while (1) {
-
-				if (sem_wait(proc_sem) == -1) {
-					perror("sem_wait");
-					exit(EXIT_FAILURE);
-				}
-				res = lseek(fd, 0, SEEK_CUR);
-				CHECK_ERR(res, "Lseek error")
-
-				read_write_in_file(&fd, i,eof);
-			}
-			break;
-		case -1:
-			perror("fork()");
-			exit(EXIT_FAILURE);
-		default:
-			;
-		}
-	}
-	for (int i = 0; i < FILE_SIZE + N; i++) {
-		if (sem_post(proc_sem) == -1) {
-			perror("sem_post");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	for (int j = 0; j < N; j++)
-		wait(NULL);
-
-	printf("Processo completato BYE!\n");
-	return;
-}
-
-//------------------------------------------------//
-
-int create_file_set_size(char *file_name, unsigned int file_size) {
-	int res;
-	int fd = open(file_name,
-	O_CREAT | O_TRUNC | O_WRONLY,
-	S_IRUSR | S_IWUSR // l'utente proprietario del file avrà i permessi di lettura e scrittura sul nuovo file
-	);
-
-	CHECK_ERR(fd, "open file")
-
-	res = ftruncate(fd, file_size);
-	CHECK_ERR(res, "ftruncate()")
-
-	return fd;
-}
-
-void read_write_in_file(int *fd, int i, int eof) {
-
-	char append_message = 'A' + (char) i;
-	char *tmp = malloc(2);
-	int res;
-	int curr;
-
-	if (sem_wait(mutex) == -1) {
-		perror("sem_wait");
-		exit(EXIT_FAILURE);
-	}
-
-	res = read(*fd, tmp, 1);
-	CHECK_ERR(res, "Read error in TMP");
-
-	if (tmp == 0) {
-		curr = lseek(*fd, 0, SEEK_CUR);
-		CHECK_ERR(res, "Lseek");
-		if (curr == eof) {
-			if (sem_post(mutex) == -1) {
-				perror("sem_post");
-				exit(EXIT_FAILURE);
-			}
-			exit(EXIT_SUCCESS);
-		}
-		printf("CHILD %d scrive a offset %d\n", i, res);
-		res = write(*fd, &append_message, sizeof(append_message));
-		CHECK_ERR(res, "Write error")
-	}
-
-	if (sem_post(mutex) == -1) {
-		perror("sem_post");
-		exit(EXIT_FAILURE);
-	}
-
-	return;
-}
